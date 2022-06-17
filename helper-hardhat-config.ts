5fa71bb2ac384ece00c203af79db4a31afb570e8
Daniel Park <Danielpark1239@gmail.com>
2022-06-17 00:59:16 -0400

    Added constants

--- helper-hardhat-config.ts
+++ helper-hardhat-config.ts
@@ -3,3 +3,10 @@ export const VOTING_PERIOD = 5
 export const VOTING_DELAY = 1
 export const QUORUM_PERCENTAGE = 4
 export const ADDRESS_ZERO = "0x0000000000000000000000000000000000000000"
+export const NEW_STORE_VALUE = 999
+export const FUNC = "store"
+export const PROPOSAL_DESCRIPTION = "Proposal #1: Store 999 in the box"
+export const PROPOSAL_NUMBER = 0
+
+export const developmentChains = ["hardhat", "localhost"]
+export const proposalsFile = "proposals.json"
